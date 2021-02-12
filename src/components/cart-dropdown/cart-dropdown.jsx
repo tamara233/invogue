8ee6d66f0fc8b83f9ee6bf9fef493ec8934c1a77
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2021-02-12 16:02:11 +0400

    created cart-item component and connected cart dropdown

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import CustomButton from '../custom-button/custom-button'
+import CartItem from '../cart-item/cart-item'
 
 import './cart-dropdown.scss'
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    {console.log('CART DROPD', cartItems, typeof cartItems)}
+    <div className="cart-items">
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
-export default CartDropdown
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.cartItems,
+})
+
+export default connect(mapStateToProps)(CartDropdown)
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,4 @@
-export const addItemToCart = (cartitems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemToAdd.id,
   )
